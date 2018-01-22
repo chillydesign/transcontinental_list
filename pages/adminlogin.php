87ba49278920816c8e26e36dc72bacb9b5e38688
Mélissa Rissel <rissel.melissa@gmail.com>
2018-01-22 15:37:00 +0000

    admin login

--- pages/adminlogin.php
+++ pages/adminlogin.php
@@ -1,14 +1,14 @@
 <div class="page_image" style="background-image:url('<?php echo get_site_url(); ?>/images/honeymoon.jpg'); overflow: hidden;"></div>
 <div class="container">
-  <h1>Login Admin</h1>
+  <h1>Connexion Administrateur</h1>
 
 <?php if (has_error()) : ?>
     <?php show_error_message(); ?>
 <?php endif; ?>
 <form action="<?php get_site_url(); ?>/actions/admin_connect.php" method="post">
 
-    <p><input type="text" name="email" placeholder="email" /></p>
-    <p><input type="password" name="password" placeholder="password" /></p>
-    <p><input type="submit" name="connect_admin" value="Submit" /></p>
+    <p><input type="text" name="email" placeholder="Email" /></p>
+    <p><input type="password" name="password" placeholder="Mot de passe" /></p>
+    <p><input type="submit" name="connect_admin" value="Connexion" /></p>
 </form>
 </div>
