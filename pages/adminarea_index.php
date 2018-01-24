3892ad72a840b960450f4f5403c69d16f5d9c9b0
Charles Harvey <harvey.charles@gmail.com>
2018-01-24 15:32:46 +0000

    added giftcard_used class to giftcard list

--- pages/adminarea_index.php
+++ pages/adminarea_index.php
@@ -60,7 +60,8 @@
             <?php include('includes/search_giftcard_form.php'); ?>
             <ul>
               <?php foreach ( get_giftcards() as $giftcard) : ?>
-                <li>
+                <?php $status = ( $giftcard->status == 'utilisé' ? 'giftcard_used' : '' ); ?>
+                <li  class="<?php echo $status ?>">
                   <a href="<?php get_site_url(); ?>/adminarea/giftcard?id=<?php echo  convert_giftcard_id($giftcard->id); ?>">
                     <strong> Bon de <?php echo convert_cents_to_currency($giftcard->amount); ?>  pour: <?php echo $giftcard->receiver_first_name; ?> <?php echo $giftcard->receiver_last_name; ?></strong><br>
                     De la part de: <?php echo $giftcard->sender_first_name; ?> <?php echo $giftcard->sender_last_name; ?>
