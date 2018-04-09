30b6dea78cbb47c1bbfaf1c7bb30f45ce990cd9b
Charles Harvey <harvey.charles@gmail.com>
2018-04-09 08:04:21 +0200

    add else if if have no lists on user index

--- pages/userarea_index.php
+++ pages/userarea_index.php
@@ -1,30 +1,34 @@
 <div class="page_image" style="background-image:url('<?php echo site_url(); ?>/images/honeymoon.jpg'); overflow: hidden;"></div>
 
 <div class="container">
-  <h1>Vos listes</h1>
-  <p>Vous pouvez consulter vos listes et leur statut ou les modifier ici.</p>
+    <h1>Vos listes</h1>
+    <?php $lists = user_lists(); ?>
+    <?php if ( sizeof($lists) > 0 ) : ?>
+        <p>Vous pouvez consulter vos listes et leur statut ou les modifier ici.</p>
+        <ul>
+            <?php foreach (  $lists as $list) : ?>
+                <li class="list_list_item">
+                    <a href="<?php get_site_url(); ?>/userarea/list?id=<?php echo $list->list_number; ?>">
+                        <div class="list_list list_list_img">
+                            <img src="<?php echo site_url();?>/images/lists/<?php echo $list->picture; ?>.jpg">
+                        </div>
+                        <div class="list_list list_list_desc">
+                            <strong><?php echo $list->name; ?></strong>
+                            <!-- (Créée le <?php  echo timeAgoInWords($list->created_at); ?>) -->
+                            <span class="creation_date">Créée le <?php  echo nice_date($list->created_at); ?></span>
+                        </div>
+                        <div class="list_list list_list_amount">
+                            <?php $donations = get_donations( $list->id , 'payé' ); ?>
+                            <?php echo sum_donations($donations); ?>
+                        </div>
+                    </a>
 
-  <ul>
-      <?php foreach (  user_lists() as $list) : ?>
-          <li class="list_list_item">
-              <a href="<?php get_site_url(); ?>/userarea/list?id=<?php echo $list->list_number; ?>">
-                <div class="list_list list_list_img">
-                  <img src="<?php echo site_url();?>/images/lists/<?php echo $list->picture; ?>.jpg">
-                </div>
-                <div class="list_list list_list_desc">
-                  <strong><?php echo $list->name; ?></strong>
-                  <!-- (Créée le <?php  echo timeAgoInWords($list->created_at); ?>) -->
-                  <span class="creation_date">Créée le <?php  echo nice_date($list->created_at); ?></span>
-                </div>
-                <div class="list_list list_list_amount">
-                    <?php $donations = get_donations( $list->id , 'payé' ); ?>
-                    <?php echo sum_donations($donations); ?>
-                </div>
-              </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else : ?>
+        <p><em>Vous n'avez pas encore de listes.</em></p>
+    <?php endif; ?>
+    <p><a class="list_button" href="<?php get_site_url(); ?>/userarea/newlist">Créer une liste</a></p>
 
-          </li>
-      <?php endforeach; ?>
-
-      <li><a class="list_button" href="<?php get_site_url(); ?>/userarea/newlist">Créer une liste</a></li>
-  </ul>
 </div>
