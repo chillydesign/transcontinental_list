ad3d7f143ee3fa7f8dc90f5064e433d8fa1b11e4
Charles Harvey <harvey.charles@gmail.com>
2018-10-09 13:32:49 +0200

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,22 @@ var minify = require('gulp-minify');
 
 
 
-gulp.task('heya', function() {
+gulp.task('heya', function(done) {
   console.log('I live! Gulp is alive!');
+      done();
 });
 
 
-gulp.task('sass', function(){
+gulp.task('sass', function(done){
   return gulp.src('scss/style.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(autoprefixer({
         cascade: false
     }))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'))
+    .pipe(gulp.dest('css'));
+    done();
+
 });
 
 
@@ -25,7 +28,7 @@ gulp.task('sass', function(){
 
 
 // Run all Gulp tasks and serve application
-gulp.task('default', ['heya', 'sass'], function() {
+gulp.task('default', gulp.series( 'sass', 'heya'), function() {
   gulp.watch('scss/*.scss', ['sass']);
 //  gulp.watch('js/*.js', ['compress']);
 });
