69db6d0c37d1f6f87a56db53596fcad86871c4d0
Charles Harvey <harvey.charles@gmail.com>
2021-11-30 18:33:02 +0000

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,32 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var minify = require('gulp-minify');
+var gulp = require("gulp");
+// var sass = require('gulp-sass');
+var sass = require("gulp-sass")(require("node-sass"));
+var autoprefixer = require("gulp-autoprefixer");
+var minify = require("gulp-minify");
 
-
-
-gulp.task('heya', function(done) {
-  console.log('I live! Gulp is alive!');
-      done();
+gulp.task("heya", function (done) {
+  console.log("I live! Gulp is alive!");
+  done();
 });
 
-
-gulp.task('sass', function(done){
-  return gulp.src('scss/style.scss')
+gulp.task("sass", function (done) {
+  return gulp
+    .src("scss/style.scss")
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(autoprefixer({
-        cascade: false
-    }))
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('css'));
-    done();
-
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    )
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("css"));
+  done();
 });
-
-
-
-
 
 // Run all Gulp tasks and serve application
-gulp.task('default', gulp.series( 'sass', 'heya'), function() {
-  gulp.watch('scss/*.scss', ['sass']);
-//  gulp.watch('js/*.js', ['compress']);
+gulp.task("default", gulp.series("sass", "heya"), function () {
+  gulp.watch("scss/*.scss", ["sass"]);
+  //  gulp.watch('js/*.js', ['compress']);
 });
 
 // Run all Gulp tasks and serve application
