be43220ba2887c9ad54e92f53de9128ea80dd604
Charles Harvey <harvey.charles@gmail.com>
2021-12-16 20:17:56 +0000

    1000 chf for giftcards max

--- actions/saferpay_generate_transaction_id.php
+++ actions/saferpay_generate_transaction_id.php
@@ -1,15 +1,15 @@
 <?php
 
 
-include('../includes/connect.php');
-include('../includes/functions.php');
+// include('../includes/connect.php');
+// include('../includes/functions.php');
 
 
-ini_set('default_charset', 'UTF-8');
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header('Content-Type: application/json;charset=UTF-8');
+// ini_set('default_charset', 'UTF-8');
+// header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+// header('Content-Type: application/json;charset=UTF-8');
 
-// $tid = generate_saferpay_transaction_id(1000);
-$order_id = 123;
-$tid = generate_saferpay_payment_page('donation', 1000, 'Description of payment', site_url() . '?test', $order_id);
-echo json_encode($tid);
+// // $tid = generate_saferpay_transaction_id(1000);
+// $order_id = 123;
+// $tid = generate_saferpay_payment_page('donation', 1000, 'Description of payment', site_url() . '?test', $order_id);
+// echo json_encode($tid);
